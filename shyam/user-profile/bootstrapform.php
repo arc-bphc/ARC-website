4316401f7335eb0fdc9f27e0b6c8a86096f431ba
yashdeep97 <yashdeep97@gmail.com>
2017-06-28 21:26:14 +0530

    minor change

--- shyam/user-profile/bootstrapform.php
+++ shyam/user-profile/bootstrapform.php
@@ -90,7 +90,7 @@ if ( isset( $_POST[ 'id' ] ) ) {
     <link rel="stylesheet" type="text/css" href="../../blog/bootstrap4/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://apis.google.com/js/platform.js" async defer></script> 
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
     <script>
       function signOut() {
       var auth2 = gapi.auth2.getAuthInstance();
