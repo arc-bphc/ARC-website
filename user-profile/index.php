196d8e007073ac3e1b5150f2217383c2ce2a9827
shaymjesal <s.jesalpura@gmail.com>
2017-07-11 21:18:28 +0530

    view all users page improved

--- user-profile/index.php
+++ user-profile/index.php
@@ -5,7 +5,26 @@ session_start();
   //echo "<b>CHECKING CONNECTION</b> <br>";
   if ( $GLOBALS[ 'con' ] ) {
 	   //echo 'Successfull <br><br>';
+    if($_SESSION["login-status"] != 1 && $_SESSION["login-status"] != 2) {
+        header("Location: ../blog/sign.php");
+    }
 
+  echo "<nav class=\"navbar navbar-toggleable-md navbar-inverse bg-inverse\">
+    <button class=\"navbar-toggler navbar-toggler-right\" type=\"button\" data-toggle=\"collapse\" data-target=\" #navbarTogglerDemo02\" aria-controls=\"navbarTogglerDemo02\" aria-expanded=\"false\" aria-label=\"Toggle   navigation\">
+      <span class=\"navbar-toggler-icon\"></span>
+     </button>
+    <div class=\"collapse navbar-collapse\" id=\"navbarTogglerDemo02\">
+     <ul class=\"navbar-nav mr-auto mt-2 mt-md-0\">
+       <li class=\"nav-item active\">
+         <a class=\"nav-link\" href=\"#\">Home <span class=\"sr-only\">(current)</span></a>
+       <li class=\"nav-item\"><a class=\"nav-link\" href=\"../blog/form.php\">Write Post</a></li>
+       <li class=\"nav-item\"><a class=\"nav-link\" href=\"../blog/display-posts.php\">Reader</a></li>
+       <li class=\"nav-item\">
+         <a class=\"nav-link\" href=\"../blog/sign.php\" onclick=\"signOut();\">Logout</a>
+       </li>
+     </ul>
+   </div>
+  </nav>";
 
   $session_id = $_SESSION['id'];                                                  //unique session id to identify loggedin user
   $qry = "SELECT * FROM `users` WHERE `ID`='$session_id'";                //
@@ -13,7 +32,12 @@ session_start();
   $user_data = $user_datalist->fetch_assoc();                             //
 
   $getList = mysqli_query($GLOBALS['con'],"SELECT * FROM users");         // fetch all user details
-  $i=1;
+  //$i=1;
+  ?>
+  <div id=bg>
+  <p style="margin: 50px; " ><h3>Members</h3></p>
+  <div class=row>
+  <?php
   while ($result = mysqli_fetch_array($getList)) {                       //loop through all the users
     $id = $result['id'];
     $name = $result['name'];
@@ -25,7 +49,7 @@ session_start();
     if(!$result['isadmin'] && $user_data['isadmin']){ $isadmin="";}                       //unique modal for every user which gets triggered when you click their picture
     echo '
           <!-- Modal -->
-              <div id="profilemodal'.$i.'" class="modal custom fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+              <div id="profilemodal'.$id.'" class="modal custom fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                 <div class="modal-dialog modal-lg" role="document">
 
           <!-- Modal content-->
@@ -65,14 +89,51 @@ session_start();
                 </div>
               </div>
 
-          <a data-toggle="modal" href="#profilemodal'.$i.'">
-
-          <img class="rounded-circle" src="'.$img_scr.'" alt="error"></a>    <!--Image to click , which will trigger respective profilemodal-->
+          <div class="card col-md-3" style="width: 20%;" style="height: 40%;">
+      <img class="card-img-top img-fluid" src="'.$img_scr.'" alt="'.$name.'">
+        <div class="card-block">
+          <h4 class="card-title">'.$name.'</h4>
+          <p class="card-text">'.$email.'</p>
+          <a href="#profilemodal'.$id.'" data-toggle="modal" class="btn btn-primary">View More</a>
+        </div>
+      </div>
           ';
-          $i++;
+          //$i++;
 	     }
+       
    }
 ?>
+</div>
+<h3 style="margin: 50px;">Admins</h3>
+<div class=row>
+<?php
+  $getList = mysqli_query($GLOBALS['con'],"SELECT * FROM users WHERE `isadmin`= 1");         // fetch all user details
+  //$i=1;
+   while ($result = mysqli_fetch_array($getList)) {                       //loop through all the users
+    $id = $result['id'];
+    $name = $result['name'];
+    $img_scr= $result['picture'];
+    $email = $result['email'];
+    echo '
+    <!--<a data-toggle="modal" href="#profilemodal'.$id.'">
+      <img class="rounded-circle" src="'.$img_scr.'" alt="error">'.$name.'</img>
+      </a>    <!--Image to click , which will trigger respective profilemodal-->
+
+      <div class="card col-md-3" style="width: 20%;" style="height: 40%;">
+      <img class="card-img-top img-fluid" src="'.$img_scr.'" alt="'.$name.'">
+        <div class="card-block">
+          <h4 class="card-title">'.$name.'</h4>
+          <p class="card-text">'.$email.'</p>
+          <a href="#profilemodal'.$id.'" data-toggle="modal" class="btn btn-primary">View More</a>
+        </div>
+      </div>
+
+      ';
+      //$i++;
+    }
+?>
+</div>
+</div>
 
   <html>
   <head>
@@ -98,7 +159,14 @@ session_start();
         xmlhttp.send();
     })
 });
-
+    
+    function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+       auth2.signOut().then(function () {
+       console.log('User signed out.');
+      });
+    }
+    
 
 
 </script>
@@ -133,16 +201,28 @@ session_start();
     height:auto;
     margin: 2%;
   }
+  .img-fluid {
+    width : 100%;
+    height: auto;
+  }
   .modal-backdrop {
    background-color: grey;
 }
 	#propic {
 		width: 50%;
 	}
+  .card {
+    background-clip: padding-box;
+    border: 10px solid transparent;
+  }
+  #bg {
+    width: 80%;
+    margin: auto;
+  }
 
 </style>
  </head>
- <body>
+ <body background-color="grey">
    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
