cc8d0ccdcb441f806c4b1bab35806cb6a93f57e5
Sakshi <shristykapoor38@gmail.com>
2017-09-18 21:29:34 +0530

    added 2 meta tags

--- home/index.php
+++ home/index.php
@@ -13,6 +13,12 @@
 <head>
   <title>ARC website</title>
   <meta charset="utf-8">
+  <meta name="keywords" content=" automation, robotics, club, science, technology, blogs, BITS, Pilani, Hyderabad, campus, robots, ATMOS"> 
+  <meta name="description" content=" The Automation and Robotics Club (ARC) is a student organization at BITS Pilani Hyderabad Campus.  We comprise people who love experimenting around with circuit boards, codes and yes, ideas. ARC was started in 20__ and has been around for quite some time now.
+             Robotics is a versatile field which is highly interdisciplinary. Even small projects involve coding, mechanical, electronics and design expertise, which means we always work in teams and all our projects are group projects.
+Our current projects include autonomous applications like chess-playing using AI, autonomous drawing robot and fields like self-navigation and self-balancing.
+Also, we proudly boast of a full scale robotic arm and a micro-quadcopter developed on our own from scratch."> 
+  
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="../blog/images/arc.png" type="image/png">
   <link rel="stylesheet" href="../blog/bootstrap4/css/bootstrap.min.css">
